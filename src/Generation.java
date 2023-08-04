0858339d1aabf0ab5635d7922f79b2378e864cb3
Mounir Zahidi <Mounir.Zahidi-1@ou.edu>
2023-08-04 22:34:11 +0100

    Implemented the Generation Class

--- src/Generation.java
+++ src/Generation.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 
 public class Generation {
 	//private members
@@ -5,22 +6,57 @@ public class Generation {
 	
 	// public members
 	Generation (boolean ... states) {
-		
+		boolean [] helper;
+		if (states == null || states.length == 0) {
+			helper = new boolean[] {false};
+		} else {
+			helper = new boolean [states.length];
+			for (int i=0; i< states.length; ++i){
+				helper[i] = states[i];
+			}
+		}
+		cellStates = Arrays.copyOf(helper, helper.length);
 	}
 	
-	Generation (String states, char trySymbol) {
-		
+	Generation (String states, char trueSymbol) {
+		boolean [] helper;
+		if (states == null || states == ""){
+			helper = new boolean[] {false};
+		} else {
+			helper = new boolean [states.length()];
+			for (int i =0; i < states.length(); ++i){
+				if (states.charAt(i)== trueSymbol){
+					helper[i] = true;
+				} else {
+					helper[i] = false;
+				}
+			}
+		}
+		cellStates = helper;
+	}
+	
+	public boolean getState(int idx) {
+		return cellStates[idx];
 	}
 	
 	public boolean [] getStates() {
-		return null;
+		return cellStates.clone();
 	}
 	
 	public String getStates(char falseSymbol, char trueSymbol) {
-		return null;
+		String result = "";
+		for (int i =0; i< cellStates.length; ++i){
+			if (cellStates[i]){
+				result += trueSymbol;
+			} else {
+				result += falseSymbol;
+			}
+		}
+		
+		return result;
 	}
 	
 	public int size() {
-		return 0;
+		return cellStates.length;
 	}
 }
--- src/Option.java
+++ src/Option.java
@@ -1,4 +1,29 @@
 
 public enum Option {
-
+	PRINT_RULE					("Show the current Rule"), 
+	REINIT_AUTOMATON			("Reinitialize Automaton"),
+	EVOLVE						("Evolve the Automaton a given number of steps"), 
+	SET_TRUE_SYMBOL				("Set the true symbol represention"),
+	SET_FALSE_SYMBOL			("Set the false symbol represention"),
+	PRINT_CURRENT_GENERATION	("Show the current Generation of the Automaton"),
+	PRINT_FULL_EVOLUTION		("Show the full evolution of the Automaton"), 
+	QUIT						("Quit");
+	
+	private String option;
+	
+	Option (String option){
+		this.option = option;
+	}
+	
+	public int index() {
+		return 0;
+	}
+	
+	public String toString() {
+		return null;
+	}
+	
+	public static Option fromInt(int x) {
+		return null;
+	}
 }
